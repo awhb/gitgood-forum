2e313abee437da5f1f341c9c6f90b5e5af44e360
awhb <awhangbin@gmail.com>
2024-06-25 02:58:29 +0800

    init app file

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
