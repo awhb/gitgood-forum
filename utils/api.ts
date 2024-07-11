ed46e7bc815c4b42fb61dd6eee9c41f5df04e6f6
awhb <awhangbin@gmail.com>
2024-07-12 02:38:44 +0800

    init api caller

---
+++ utils/api.ts
@@ -0,0 +1,19 @@
+import { cookies } from 'next/headers';
+
+export async function fetchApi(endpoint: string, options: RequestInit = {}) {
+  try {
+    const res = await fetch(`${process.env.BACKEND_API_URL}${endpoint}`, {
+      ...options,
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `${cookies().get('Authorization')?.value}`,
+        ...options.headers,
+      },
+    });
+    return res;
+    
+  } catch (error) {
+    throw error;
+  }
+}
