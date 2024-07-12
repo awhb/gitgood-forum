476949b2fe3ab290474d8f3de1462cea4b738477
awhb <awhangbin@gmail.com>
2024-07-12 16:50:40 +0800

    type handling for no user (edge case)

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,20 +1,16 @@
-'use server'
+'use server';
 
-import { cookies } from 'next/headers';
-import { fetchApi } from '@/utils/api';
 import { User } from '@/models/models';
 import { getUser } from '@/app/actions';
 
 export default async function ProfilePage({ params }: { params: { username: string } }) {
-    const user = await getUser() as User;
-
-    // TODO: check user in params.username
+    const user = await getUser() as User | null;
     
     return (
         <div>
             <h1 className="text-2xl font-bold my-4">Profile</h1>
             <p className="mb-4">Username: {params.username}</p>
-            { user.username === params.username && <p>You are logged in as {user.username}</p> }
+            { user && user.username === params.username && <p>You are logged in as {user.username}</p> }
         </div>
     );
 }
