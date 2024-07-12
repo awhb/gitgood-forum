abf69ee6b96d737cede155ed4d89002adb3a9b33
awhb <awhangbin@gmail.com>
2024-07-12 16:52:09 +0800

    make threadlist server deployed

--- components/ThreadList.tsx
+++ components/ThreadList.tsx
@@ -1,20 +1,40 @@
-'use client';
+'use server';
+
 import Link from 'next/link';
 import { Thread } from '@/models/models';
-import { useState, useEffect } from 'react';
 import { fetchApi } from '@/utils/api';
 
-export default function ThreadList({ threads }: { threads: Thread[] }) {
-    
+export default async function ThreadList() {
+  let threads: Thread[] = [];
+
+  try {
+    const res = await fetchApi('/threads', { method: 'GET' });
+    const data = await res.json();
+
+    if (res.ok && data.threads) {
+      threads = data.threads as Thread[];
+    } else {
+      console.error('Error fetching threads:', data.error);
+    }
+  } catch (error) {
+    console.error('Error fetching threads: unknown error', error);
+  }
+
+
   return (
-    <ul className="space-y-2">
-      {threads.map((thread) => (
-        <li key={thread.id} className="border p-2 rounded">
-          <Link href={`/threads/${thread.id}`}>
-            {thread.title}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      { threads.length === 0 ?
+        <p>No threads found.</p> : 
+        <ul className="space-y-2">
+        {threads.map((thread) => (
+          <li key={thread.id} className="border p-2 rounded">
+            <Link href={`/threads/${thread.id}`}>
+              {thread.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      }
+    </>
   );
 }
