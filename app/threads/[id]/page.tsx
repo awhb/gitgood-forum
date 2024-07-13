1d8c48e0ca0f2bc9303e81ca7cdb97194377916d
awhb <awhangbin@gmail.com>
2024-07-13 18:47:24 +0800

    adjust thread view component

--- app/threads/[id]/page.tsx
+++ app/threads/[id]/page.tsx
@@ -1,17 +1,19 @@
 'use server';
 
-// import CommentList from '@/components/CommentList';
-// import CommentForm from '@/components/CommentForm';
-
 import { fetchApi } from '@/utils/api';
 import { Thread } from '@/models/models';
+import { getUser } from '@/app/actions';
+import Link from 'next/link';
+import CommentList from '@/components/CommentList';
+import CommentForm from './CommentForm';
 
 export default async function ThreadDetail({ params }: { params: { id: string } }) {
   // Fetch thread details from API
   let thread = null;
+  const user = await getUser();
   
   try {
-    const res = await fetchApi(`/threads/${params.id}`, { method: 'GET' });
+    const res = await fetchApi(`/threads/${params.id}`, { method: 'GET', cache: 'no-store'});
     const data = await res.json();
     if (res.ok && data.thread) {
       thread = data.thread as Thread;
@@ -23,16 +25,31 @@ export default async function ThreadDetail({ params }: { params: { id: string } 
   }
 
   return (
-    <div className="container mx-auto px-4">
-      { !thread ? <p>No thread found.</p>
-        : <>
-          <p className="text-gray-500">Posted by {thread.user.username}</p>
-          <h1 className="text-3xl font-bold my-4">{thread.title}</h1>
-          <p className="text-gray-500">Upvotes: {thread.upvotes}</p>
-          <p>{thread.content}</p>
-          {/* <CommentList comments={thread.comments} /> */}
-          {/* <CommentForm /> */}
-        </>
+    <div className="max-w-3xl mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
+      { !thread 
+          ? <p className="text-center text-gray-600">No thread found.</p>
+          : !user 
+            ? <p className="text-center text-red-600 font-semibold">Unauthorized!</p>
+            : <>
+              <div className="mb-4 flex justify-between items-center">
+                <p>Posted by <span className="font-semibold">{thread.user.username}</span></p>
+                {user.ID === thread.user_id 
+                  && (<Link href={`/threads/${params.id}/edit`} className="text-blue-600 hover:underline">
+                    Edit
+                  </Link>
+                )}
+              </div>
+              <h1 className="text-2xl font-bold mb-4">{thread.title}</h1>
+              <div className="flex items-center mb-4">
+                <span className="mr-2 text-gray-600">Upvotes:</span>
+                <span className="font-semibold">{thread.upvotes}</span>
+              </div>
+              <div className="prose max-w-none mb-8">{thread.content}</div>
+              <hr className="my-8" />
+              <h2 className="text-xl font-semibold mb-4">Comments</h2>
+              <CommentList comments={thread.comments} />
+              <CommentForm thread_id={thread.ID} />
+            </>
       }
     </div>
   );
