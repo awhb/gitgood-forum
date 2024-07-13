8189b0c1796e708404a64b27d5844c2a87ec9bf5
awhb <awhangbin@gmail.com>
2024-07-13 11:21:45 +0800

    create thread view

--- app/threads/[id]/page.tsx
+++ app/threads/[id]/page.tsx
@@ -3,16 +3,36 @@
 // import CommentList from '@/components/CommentList';
 // import CommentForm from '@/components/CommentForm';
 
+import { fetchApi } from '@/utils/api';
+import { Thread } from '@/models/models';
+
 export default async function ThreadDetail({ params }: { params: { id: string } }) {
   // Fetch thread details from API
-
+  let thread = null;
+  
+  try {
+    const res = await fetchApi(`/threads/${params.id}`, { method: 'GET' });
+    const data = await res.json();
+    if (res.ok && data.thread) {
+      thread = data.thread as Thread;
+    } else {
+      console.error('Error fetching thread:', data.error);
+    }
+  } catch (error) {
+    console.error('Error fetching thread: unknown error', error);
+  }
 
   return (
     <div className="container mx-auto px-4">
-      <h1 className="text-2xl font-bold my-4">Thread Title</h1>
-      <p className="mb-4">Thread content goes here...</p>
-      {/* <CommentList threadId={params.id} />
-      <CommentForm threadId={params.id} /> */}
+      { thread ? 
+        <>
+          <h1 className="text-3xl font-bold my-4">{thread.title}</h1>
+          <p>{thread.content}</p>
+        </>
+        : <p>No thread found.</p>
+      }
+      {/* <CommentList /> */}
+      {/* <CommentForm /> */}
     </div>
   );
 }
